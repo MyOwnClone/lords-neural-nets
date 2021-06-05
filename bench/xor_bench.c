706ae4f538e1bc9070a25b5c6cbaf88ad473feb3
Tomas Vymazal <tomas.vymazal@gmail.com>
2021-06-05 13:32:36 +0200

    xor float finally trains "good enough". Speed still terrible.

---
+++ bench/xor_bench.c
@@ -0,0 +1,123 @@
+#include "../lib/network.h"
+#include "../lib/functions.h"
+#include "../lib/utils.h"
+#include "../lib/matrix.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "bench_utils.h"
+
+void xor_float()
+{
+    int layers[] = {2,1};
+
+    Activation *act_sigmoid = create_sigmoid_activation();
+    Network *xor_network = create_network(2, 2, layers, act_sigmoid, D_FLOAT);
+
+    Matrix **inputs = (Matrix**) malloc (sizeof (Matrix*) * 4);
+    float inputs_mat[4][2][1] = {
+            {{1}, {1}},
+            {{1}, {0}},
+            {{0}, {1}},
+            {{0}, {0}}
+    };
+
+    Matrix **labels = (Matrix**) malloc (sizeof (Matrix*) * 4);
+    float labels_mat[4][1][1] = {
+            {{0}},
+            {{1}},
+            {{1}},
+            {{0}}
+    };
+
+    for (int i = 0; i < 4; i++)
+    {
+        inputs[i] = create_f_matrix(2, 1, inputs_mat[i]);
+        labels[i] = create_f_matrix(1, 1, labels_mat[i]);
+    }
+
+    Monitor monitor[] = {acc, loss};
+    Dataset *dataset = create_dataset(4,2,1,4, inputs, labels, NULL, NULL);
+
+    CostType cost_type = CROSS_ENTROPY;
+
+    TrainingOptions *training_options = init_training_options();
+    training_options->cost_type = cost_type;
+    training_options->epochs = 2000;
+    training_options->batch_size = 0;
+    training_options->learning_rate = 1;
+    training_options->momentum = 0.9;
+    training_options->regularization_lambda = 0.0001;
+
+    train_f(xor_network, dataset, monitor, training_options);
+
+    delete_network(xor_network);
+    delete_activation(act_sigmoid);
+    delete_dataset(dataset);
+    delete_training_options(training_options);
+}
+
+void xor_double()
+{
+    int layers[] = {2,1};
+
+    Activation *act_sigmoid = create_sigmoid_activation();
+    Network *xor_network = create_network(2, 2, layers, act_sigmoid, D_DOUBLE);
+
+    Matrix **inputs = (Matrix**) malloc (sizeof (Matrix*) * 4);
+    double inputs_mat[4][2][1] = {
+            {{1}, {1}},
+            {{1}, {0}},
+            {{0}, {1}},
+            {{0}, {0}}
+    };
+
+    Matrix **labels = (Matrix**) malloc (sizeof (Matrix*) * 4);
+    double labels_mat[4][1][1] = {
+            {{0}},
+            {{1}},
+            {{1}},
+            {{0}}
+    };
+
+    for (int i = 0; i < 4; i++)
+    {
+        inputs[i] = create_d_matrix(2, 1, inputs_mat[i]);
+        labels[i] = create_d_matrix(1, 1, labels_mat[i]);
+    }
+
+    Monitor monitor[] = {acc, loss};
+    Dataset *dataset = create_dataset(4,2,1,4, inputs, labels, NULL, NULL);
+
+    CostType cost_type = CROSS_ENTROPY;
+
+    TrainingOptions *training_options = init_training_options();
+    training_options->cost_type = cost_type;
+    training_options->epochs = 2000;
+    training_options->batch_size = 0;
+    training_options->learning_rate = 1;
+    training_options->momentum = 0.9;
+    training_options->regularization_lambda = 0.0001;
+
+    train(xor_network, dataset, monitor, training_options);
+
+    delete_network(xor_network);
+    delete_activation(act_sigmoid);
+    delete_dataset(dataset);
+    delete_training_options(training_options);
+}
+
+int main()
+{
+    double float_msecs = print_elapsed_time(xor_float, "xor float", 1);
+    double double_msecs = print_elapsed_time(xor_double, "xor double", 1);
+
+    printf("float over double speed-up factor: %fx\n", (double_msecs / float_msecs));
+
+    // mingw 64
+
+    /*
+     *  xor float: Average time elapsed over 1 runs: 9582.000000 ms :-(
+     *  xor double: Average time elapsed over 1 runs: 8743.000000 ms
+        float over double speed-up factor: 0.912440x
+     */
+}
