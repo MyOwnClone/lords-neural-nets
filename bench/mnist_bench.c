7295357d9c2c5e420f8d855f8a85b01e1957ea25
Tomas Vymazal <tomas.vymazal@gmail.com>
2021-07-07 10:40:03 +0200

    wip

--- bench/mnist_bench.c
+++ bench/mnist_bench.c
@@ -15,22 +15,22 @@ void mnist_double()
     char *train_inputs_fn = "./resources/mnist_train_vectors.csv";
     Matrix **train_inputs = load_csv(train_inputs_fn, num_train, 28*28, D_DOUBLE);
     normalize(train_inputs, num_train, 255);
-    logger(INFO, __func__, "Created training dataset");
+    logger(LOG_INFO, __func__, "Created training dataset");
 
     char *train_labels_fn = "./resources/mnist_train_labels.csv";
     Matrix **train_labels = load_csv(train_labels_fn, num_train, 1, D_DOUBLE);
     vectorize(train_labels, num_train, 10);
-    logger(INFO, __func__, "Created training labels dataset");
+    logger(LOG_INFO, __func__, "Created training labels dataset");
 
     char *test_inputs_fn = "./resources/mnist_test_vectors.csv";
     Matrix **test_inputs = load_csv(test_inputs_fn, num_test, 28*28, D_DOUBLE);
     normalize(test_inputs, num_test, 255);
-    logger(INFO, __func__, "Created test dataset");
+    logger(LOG_INFO, __func__, "Created test dataset");
 
     char *test_labels_fn = "./resources/mnist_test_labels.csv";
     Matrix **test_labels = load_csv(test_labels_fn, num_test, 1, D_DOUBLE);
     vectorize(test_labels, num_test, 10);
-    logger(INFO, __func__, "Created test labels dataset");
+    logger(LOG_INFO, __func__, "Created test labels dataset");
 
 
     Dataset *dataset = create_dataset(num_train, num_test, train_inputs, train_labels, test_inputs, test_labels);
@@ -72,25 +72,25 @@ void mnist_float()
 
     normalize(train_inputs, num_train, 255);
 
-    logger(INFO, __func__, "Created training dataset");
+    logger(LOG_INFO, __func__, "Created training dataset");
 
     char *train_labels_fn = "./resources/mnist_train_labels.csv";
     Matrix **train_labels = load_csv(train_labels_fn, num_train, 1, D_FLOAT);
 
     vectorize(train_labels, num_train, 10);
-    logger(INFO, __func__, "Created training labels dataset");
+    logger(LOG_INFO, __func__, "Created training labels dataset");
 
     char *test_inputs_fn = "./resources/mnist_test_vectors.csv";
     Matrix **test_inputs = load_csv(test_inputs_fn, num_test, 28*28, D_FLOAT);
 
     normalize(test_inputs, num_test, 255);
-    logger(INFO, __func__, "Created test dataset");
+    logger(LOG_INFO, __func__, "Created test dataset");
 
     char *test_labels_fn = "./resources/mnist_test_labels.csv";
     Matrix **test_labels = load_csv(test_labels_fn, num_test, 1, D_FLOAT);
 
     vectorize(test_labels, num_test, 10);
-    logger(INFO, __func__, "Created test labels dataset");
+    logger(LOG_INFO, __func__, "Created test labels dataset");
 
     Dataset *dataset = create_dataset(num_train, num_test, train_inputs, train_labels, test_inputs, test_labels);
     Metrics metrics;
