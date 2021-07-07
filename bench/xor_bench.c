9b510bdff771ef5aebd510498c9162a4b761a1a0
Tomas Vymazal <tomas.vymazal@gmail.com>
2021-07-07 11:21:59 +0200

    wip

--- bench/xor_bench.c
+++ bench/xor_bench.c
@@ -3,49 +3,73 @@
 #include "bench_utils.h"
 #include "../lib/network.h"
 
+static int xor_neurons_per_layer[] = {2, 1};
+
+#define BINARY_OPERAND_COUNT 2
+#define XOR_COMBINATION_COUNT (2 * 2)
+
 // TODO: refactor
+
+static const int XOR_EPOCH_COUNT = 20000;
+static const int XOR_BATCH_SIZE = 0;
+static const float XOR_LEARNING_RATE = 1;
+static const double XOR_MOMENTUM = 0.9;
+
+static const double XOR_REG_LAMBDA = 0.0001;
+
+void delete_train_data(Activation *act_sigmoid, Network *xor_network, Dataset *dataset, TrainingOptions *training_options, TrainingLoggingOptions *training_logging_options)
+{
+    delete_network(xor_network);
+    delete_activation(act_sigmoid);
+    delete_dataset(dataset);
+    delete_training_options(training_options);
+    delete_training_logging_options(training_logging_options);
+}
+
+void set_training_options(CostType cost_type, TrainingOptions *training_options)
+{
+    training_options->cost_type = cost_type;
+    training_options->epochs = XOR_EPOCH_COUNT;
+    training_options->batch_size = XOR_BATCH_SIZE;
+    training_options->learning_rate = XOR_LEARNING_RATE;
+    training_options->momentum = XOR_MOMENTUM;
+    training_options->regularization_lambda = XOR_REG_LAMBDA;
+}
 
 void xor_float()
 {
-    int layers[] = {2,1};
-
     Activation *act_sigmoid = create_sigmoid_activation();
-    Network *xor_network = create_network(2, 2, layers, act_sigmoid, D_FLOAT, -1);
+    Network *xor_network = create_network(BINARY_OPERAND_COUNT, 2, xor_neurons_per_layer, act_sigmoid, D_FLOAT, -1);
 
-    Matrix **inputs = (Matrix**) malloc (sizeof (Matrix*) * 4);
-    float inputs_mat[4][2][1] = {
+    Matrix **inputs = (Matrix**) malloc (sizeof (Matrix*) * XOR_COMBINATION_COUNT);
+    float inputs_mat[XOR_COMBINATION_COUNT][BINARY_OPERAND_COUNT][1] = {
             {{1}, {1}},
             {{1}, {0}},
             {{0}, {1}},
             {{0}, {0}}
     };
 
-    Matrix **labels = (Matrix**) malloc (sizeof (Matrix*) * 4);
-    float labels_mat[4][1][1] = {
+    Matrix **labels = (Matrix**) malloc (sizeof (Matrix*) * XOR_COMBINATION_COUNT);
+    float labels_mat[XOR_COMBINATION_COUNT][1][1] = {
             {{0}},
             {{1}},
             {{1}},
             {{0}}
     };
 
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < XOR_COMBINATION_COUNT; i++)
     {
-        inputs[i] = create_matrix_f(2, 1, inputs_mat[i]);
+        inputs[i] = create_matrix_f(BINARY_OPERAND_COUNT, 1, inputs_mat[i]);
         labels[i] = create_matrix_f(1, 1, labels_mat[i]);
     }
 
     Metrics metrics;
-    Dataset *dataset = create_dataset(4, 4, inputs, labels, NULL, NULL);
+    Dataset *dataset = create_dataset(XOR_COMBINATION_COUNT, XOR_COMBINATION_COUNT, inputs, labels, NULL, NULL);
 
     CostType cost_type = CROSS_ENTROPY;
 
     TrainingOptions *training_options = init_training_options();
-    training_options->cost_type = cost_type;
-    training_options->epochs = 20000;
-    training_options->batch_size = 0;
-    training_options->learning_rate = 1;
-    training_options->momentum = 0.9;
-    training_options->regularization_lambda = 0.0001;
+    set_training_options(cost_type, training_options);
 
     TrainingLoggingOptions *training_logging_options = init_training_logging_options();
 #ifdef DEBUG_MODE
@@ -56,54 +80,43 @@ void xor_float()
 
     train_f(xor_network, dataset, &metrics, training_options, training_logging_options);
 
-    delete_network(xor_network);
-    delete_activation(act_sigmoid);
-    delete_dataset(dataset);
-    delete_training_options(training_options);
-    delete_training_logging_options(training_logging_options);
+    delete_train_data(act_sigmoid, xor_network, dataset, training_options, training_logging_options);
 }
 
 void xor_double()
 {
-    int layers[] = {2,1};
-
     Activation *act_sigmoid = create_sigmoid_activation();
-    Network *xor_network = create_network(2, 2, layers, act_sigmoid, D_DOUBLE, -1);
+    Network *xor_network = create_network(BINARY_OPERAND_COUNT, 2, xor_neurons_per_layer, act_sigmoid, D_DOUBLE, -1);
 
-    Matrix **inputs = (Matrix**) malloc (sizeof (Matrix*) * 4);
-    double inputs_mat[4][2][1] = {
+    Matrix **inputs = (Matrix**) malloc (sizeof (Matrix*) * XOR_COMBINATION_COUNT);
+    double inputs_mat[XOR_COMBINATION_COUNT][BINARY_OPERAND_COUNT][1] = {
             {{1}, {1}},
             {{1}, {0}},
             {{0}, {1}},
             {{0}, {0}}
     };
 
-    Matrix **labels = (Matrix**) malloc (sizeof (Matrix*) * 4);
-    double labels_mat[4][1][1] = {
+    Matrix **labels = (Matrix**) malloc (sizeof (Matrix*) * XOR_COMBINATION_COUNT);
+    double labels_mat[XOR_COMBINATION_COUNT][1][1] = {
             {{0}},
             {{1}},
             {{1}},
             {{0}}
     };
 
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < XOR_COMBINATION_COUNT; i++)
     {
-        inputs[i] = create_matrix_d(2, 1, inputs_mat[i]);
+        inputs[i] = create_matrix_d(BINARY_OPERAND_COUNT, 1, inputs_mat[i]);
         labels[i] = create_matrix_d(1, 1, labels_mat[i]);
     }
 
     Metrics metrics;
-    Dataset *dataset = create_dataset(4, 4, inputs, labels, NULL, NULL);
+    Dataset *dataset = create_dataset(XOR_COMBINATION_COUNT, XOR_COMBINATION_COUNT, inputs, labels, NULL, NULL);
 
     CostType cost_type = CROSS_ENTROPY;
 
     TrainingOptions *training_options = init_training_options();
-    training_options->cost_type = cost_type;
-    training_options->epochs = 20000;
-    training_options->batch_size = 0;
-    training_options->learning_rate = 1;
-    training_options->momentum = 0.9;
-    training_options->regularization_lambda = 0.0001;
+    set_training_options(cost_type, training_options);
 
     TrainingLoggingOptions *training_logging_options = init_training_logging_options();
 #ifdef DEBUG_MODE
@@ -114,11 +127,7 @@ void xor_double()
 
     train_d(xor_network, dataset, &metrics, training_options, training_logging_options);
 
-    delete_network(xor_network);
-    delete_activation(act_sigmoid);
-    delete_dataset(dataset);
-    delete_training_options(training_options);
-    delete_training_logging_options(training_logging_options);
+    delete_train_data(act_sigmoid, xor_network, dataset, training_options, training_logging_options);
 }
 
 int main()
